e3b42e678e6e47a18f8e542162a8e19621443ee6
Rafał W <rafal.widlo@gmail.com>
2021-01-14 00:06:54 +0100

    Code update related to changes data structure
    Colored output removed in favor of the data table. Added support for a different OSs to clear the console screen after latest data is fetched.

--- main.py
+++ main.py
@@ -1,50 +1,33 @@
-from colorama import init, Fore, Back, Style
 from bs4 import BeautifulSoup
+import prettytable
 import requests
 import time
 import os
+import platform
 import json
 
-init()
 website = "https://www.gov.pl/web/koronawirus/wykaz-zarazen-koronawirusem-sars-cov-2"
+system = platform.system()
+if system == "Windows":
+    clear = "cls"
+elif system in ["Darwin", "Linux"]:
+    clear = "clear"
 
-colors = [Fore.RED, Fore.GREEN, Fore.YELLOW,
-          Fore.BLUE, Fore.MAGENTA, Fore.CYAN, Fore.WHITE]
-
+os.system(clear)
 
 while True:
-    try:
-        print("REFRESHING ", end="")
-        time.sleep(1)
-        print("-> DOWNLOADING ", end="")
-        time.sleep(1)
-        page = requests.get(website)
-        print("-> PARSING ", end="")
-        time.sleep(1)
-        soup = BeautifulSoup(page.text, "html.parser")
-        print("-> FILTERING ", end="")
-        os.system("cls")
-        time.sleep(1)
-        data = soup.find(id="registerData")
-        data = data.text
-        data = json.loads(data)
-        print(data["description"])
-        print()
-        data = json.loads(data["parsedData"])
-        namelength = 0
-        countlength = 0
-        deadlength = 0
-        for dataline in data:
-            if(len(str(dataline["Województwo"]))+2) > namelength:
-                namelength = len(str(dataline["Województwo"]))+2
-            if(len(str(dataline["Liczba"]))) > countlength:
-                countlength = len(str(dataline["Liczba"]))
-            if(len(str(dataline["Liczba zgonów"]))) > deadlength:
-                deadlength = len(str(dataline["Liczba zgonów"]))
-        for index, dataline in enumerate(data):
-            print(colors[(index % len(colors))], "[", str(dataline["Województwo"]).title(), "]: ", " "*(namelength - len(str(dataline["Województwo"]))-2), "Liczba: ",
-                  dataline["Liczba"], " "*(countlength - len(str(dataline["Liczba"]))), " Liczba zgonów: ", " "*(deadlength - len(str(dataline["Liczba zgonów"]))), dataline["Liczba zgonów"], sep="")
-        time.sleep(60)
-        print(Fore.RESET, end="")
-    except Exception as e:
-        print(e)
+    data = [line.split(";") for line in json.loads(BeautifulSoup(requests.get(
+        website).text, "html.parser").find(id="registerData").text)["data"].split("\r\n")]
+
+    table = prettytable.PrettyTable()
+
+    table.field_names = data[0][:-1]
+
+    for index, data_chunk in enumerate(data[1:-1]):
+
+        print(index)
+        table.add_row(data_chunk[:-1])
+
+    print(table)
+    time.sleep(60)
+    os.system(clear)
